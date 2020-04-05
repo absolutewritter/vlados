0f205530dbf943d3d1208a36324acc4dfcd6074f
Absolute_coder <55507669+absolutewritter@users.noreply.github.com>
2020-04-06 01:36:50 +0300

    Update autoupdate.sh

--- dev/autoupdate.sh
+++ dev/autoupdate.sh
@@ -1,4 +1,4 @@
-cd '/home/vlad-hacker/Рабочий стол/c/kernelDir/'
+cd 'dir of kernel'
 nasm -f elf32 kernel.asm -o kasm.o
 gcc -m32 -c kernel.c -o kc.o
 ld -m elf_i386 -T link.ld -o kernel kasm.o kc.o
